e9bf5f1aaa83fcf3ba51c094e3f8a3d31596b7b6
starrysky51 <gz.51.pt@gmail.com>
2019-03-14 00:12:38 +0900

    読み込んだ身長と体重からBMIを求めて表示

---
+++ main2-13.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main (void){
+    double height, weight;
+
+    printf("身長を入力してください："); scanf("%lf", &height);
+    printf("体重を入力してください："); scanf("%lf", &weight);
+
+    printf("BMIは%.2fです\n", weight / ((height / 100.0) * (height / 100.0)));
+
+    return 0;
+}
