1b1c7e34a87bc31e1c44afaba9b53f1317b0fcf5
starrysky51 <gz.51.pt@gmail.com>
2019-03-14 00:24:59 +0900

    読み込んだ整数値BはAの約数か

---
+++ main3-2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main (void){
+    int a, b;
+
+    puts("２つの整数を入力してください。");
+    printf("整数A："); scanf("%d", &a);
+    printf("整数B："); scanf("%d", &b);
+
+    if (a % b)
+        puts("BはAの約数ではありません。");
+    else 
+        puts("BはAの約数です。");
+    
+    return 0;
+}
