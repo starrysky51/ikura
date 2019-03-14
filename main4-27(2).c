0fa7d1c61f9b8f18e9ecd5748f1785ed4886d4db
starrysky51 <gz.51.pt@gmail.com>
2019-03-14 22:47:04 +0900

    左上が直角の直角二等辺三角形を表示

---
+++ main4-27(2).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main (void) {
+    int i, j, len;
+
+    puts("左上直角二等辺三角形を作ります。");
+    printf("短辺：");
+    scanf("%d", &len);
+
+    for (i = len; i >= 1; i--) {
+        for (j = 1; j <= 1; j++)
+            putchar('*');
+        putchar('\n');
+    }
+    return 0;
+}
