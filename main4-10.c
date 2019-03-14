2d9e944b4741df8ceb30feb4c62a4a3d1501c2e7
starrysky51 <gz.51.pt@gmail.com>
2019-03-14 15:52:18 +0900

    読み込んだ正の整数値の個数だけ＋とーを交互に表示

---
+++ main4-10.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main (void){
+    int i, no;
+
+    printf("正の整数：");
+    scanf("%d", &no);
+
+    if (no >= 1) {
+        int i = 1;
+        while (i <= no) {
+            if (i % 2)
+                putchar('+');
+            else 
+                putchar('-');
+            i++;
+        }
+        putchar('\n');
+    }
+    return 0;
+}
