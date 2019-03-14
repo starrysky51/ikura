55e4d43a263beb21fb5c96b97f2108b83db66cd8
starrysky51 <gz.51.pt@gmail.com>
2019-03-14 15:23:01 +0900

    読み込んだ正の整数値までカウントアップ

---
+++ main4-5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main (void){
+    int i, no;
+
+    printf("正の整数を入力してください：");
+    scanf("%d", &no);
+
+    if (no >= 1){
+        int i = 1;
+        while (i <= no)
+            printf("%d", i++);
+        printf("\n");
+    }
+
+    return 0;
+}
