00879863ad8ff63f92db7da1fcd4cb183e6c0da8
starrysky51 <gz.51.pt@gmail.com>
2019-03-16 01:36:40 +0900

    Update main5-1.c

--- main5-1.c
+++ main5-1.c
@@ -1,15 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main (void){
+int main(void)
+{
     int i;
     int v[5];
 
     for (i = 0; i < 5; i++)
         v[i] = i;
-    
+
     for (i = 0; i < 5; i++)
         printf("v[%d] = %d\n", i, v[i]);
-    
+
     return 0;
-    
 }
